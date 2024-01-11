5e9b0c3650f2775e3e52df5ddf7715a3e1d8c4a8
Pedro Esteves <kropinho211@outlook.com>
2024-01-11 11:37:51 -0300

    "feature to find user, update password and get all users"

--- park.api/src/main/java/com/compass/park/api/service/UsuarioService.java
+++ park.api/src/main/java/com/compass/park/api/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -15,5 +17,24 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }

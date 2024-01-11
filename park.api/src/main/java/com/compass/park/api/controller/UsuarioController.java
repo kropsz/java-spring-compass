a25f25ca26bc655e373ccb3ea8838afd936c27dd
Pedro Esteves <kropinho211@outlook.com>
2024-01-11 10:38:50 -0300

    "user repository, service and controller created"

---
+++ park.api/src/main/java/com/compass/park/api/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.compass.park.api.controller;
+
+import com.compass.park.api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+}
---
+++ park.api/src/main/java/com/compass/park/api/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.compass.park.api.service;
+
+import com.compass.park.api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+}

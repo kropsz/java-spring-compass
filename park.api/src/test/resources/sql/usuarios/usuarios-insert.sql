0ccb579ec054a687173cffed4e3c7c57e8309e81
Pedro Esteves <kropinho211@outlook.com>
2024-01-13 10:18:16 -0300

    "test for create new user"

---
+++ park.api/src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO TB_USUARIOS (id, username, password, role)
+    VALUES (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN');
+
+INSERT INTO TB_USUARIOS (id, username, password, role)
+    VALUES (101, 'bia@gmail.com', '123456', 'ROLE_CLIENTE');
+
+INSERT INTO TB_USUARIOS (id, username, password, role)
+    VALUES (102, 'bob@gmail.com', '123456', 'ROLE_ADMIN');
